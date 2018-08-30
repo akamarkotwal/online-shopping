6f2dc77d008f231564db998bd3bce79e513c73b2
amar kotwal <akamarkotwal@gmail.com>
2018-08-30 15:29:26 +0530

    completed theme and interface issue

---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,19 @@
+$(function(){
+	switch(menu){
+		
+		case 'About Us':
+			$('#about').addClass('active');
+			break;
+		case 'Contact us':
+			$('#contact').addClass('active');
+			break;
+		default: 
+			$('#home').addClass('active');
+			break;
+	
+	}
+	
+	
+	
+	
+});
